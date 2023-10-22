0626447c1dfdf5ee3b826e9caf06f5fa6fed224a
= <48965957+Louis-Amand@users.noreply.github.com>
2023-10-22 22:05:02 +0200

    Correcting errors and adding other messages

--- routes/email.js
+++ routes/email.js
@@ -3,14 +3,22 @@ const router = express.Router();
 const emailService = require('../services/emailService');
 
 router.post('/send', (req, res) => {
-    const { userEmailAddress, message } = req.body;
+    const { name, firstname, phone, mail, message } = req.body;
 
     console.log(req.body);
 
-    emailService.sendEmail(message, (error, info) => {
+    const formattedMessage = `
+        Nom: ${name}
+        Prénom: ${firstname}
+        Téléphone: ${phone}
+        Email: ${mail}
+        Message: ${message}
+    `;
+
+    emailService.sendEmail(formattedMessage, (error, info) => {
         if (error) {
             console.log(error);
-            res.status(500).json({ message: 'Erreur lors de l\'envoi de l\'e-mail' });
+            res.status(500).json({ message: 'Erreur lors de l\'envoi de l\'e-mail'+error+' ------- '+info });
         } else {
             console.log('Email envoyé: ' + info.response);
             res.status(200).json({ message: 'Email envoyé' });
--- services/emailService.js
+++ services/emailService.js
@@ -1,15 +1,5 @@
 const nodemailer = require('nodemailer');
 
-// const transporter = nodemailer.createTransport({
-//     host: 'smtp.zoho.com',
-//     port: 465,
-//     secure: true,  // true for 465, false for other ports
-//     auth: {
-//         user: process.env.EMAIL_USER,
-//         pass: process.env.EMAIL_PASS
-//     }
-// });
-
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -23,7 +13,7 @@ function sendEmail(message, callback) {
         from: process.env.EMAIL_USER,
         to: "louisamandd@gmail.com",
         subject: "[Portfolio] Formulaire de contact",
-        text: message,
+        text:  message,
     };
     console.log("mailOptions", mailOptions);
 
